ee15298fd7190c6ab58235edc3fb4b48df75272c
Miller-Kodish <135719527+Miller-Kodish@users.noreply.github.com>
2023-06-08 15:36:44 -0500

    Edited Day One Material

---
+++ src/com/mit/trainingDayOne/AbstractionExampleOne.java
@@ -0,0 +1,12 @@
+package com.mit.trainingDayOne;
+
+public class AbstractionExampleOne {
+    // Abstract means hiding
+    // Abstraction is showing only the things you need to show
+
+    private String welcomeMessage = "Hello, World!";
+
+    public static void main(String[] args) {
+
+    }
+}

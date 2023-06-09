9959e450e800505dc1c09635e9605e3a8f00b8d9
Miller-Kodish <135719527+Miller-Kodish@users.noreply.github.com>
2023-06-09 09:01:04 -0500

    Added checking case for negative numbers

--- src/com/mit/trainingDayTwo/PrimeNumberChecker.java
+++ src/com/mit/trainingDayTwo/PrimeNumberChecker.java
@@ -14,13 +14,14 @@ public class PrimeNumberChecker {
 
         number = myObj.nextInt();   // Set input to variable
 
-        // Check edge cases of 0, and 1
+        // Check edge cases of 0 and 1, as well as negative numbers
         if (number == 0) {
             System.out.println("Number entered in is 0, which is NEITHER PRIME NOR COMPOSITE.");
         } else if (number == 1) {
             System.out.println("Number entered in is 1, which is NEITHER PRIME NOR COMPOSITE.");
+        } else if (number < 0) {
+            System.out.println("ERROR: NUMBER IS NEGATIVE, INVALID INPUT (PLEASE TRY AGAIN).");
         } else {    // If not an edge case, evaluate normally
-            outer:
             for (i = 2; i < number; i++) {
                 if ((number % i) == 0) {
                     isComposite = true; // Set composite value to true
